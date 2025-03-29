d7002b42f2e6852c9b8998365821db017a4fe2c8
VINCENT <vwal290@aucklanduni.ac.nz>
2025-03-30 11:35:02 +1300

    Update App component to use a Button and enhance layout styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
-      <div>
-        <p>Hello world!</p>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
       </div>
     </>
   );
