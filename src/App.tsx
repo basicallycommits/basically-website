459db4e7d41665cc7fc7d2326cc14695abe3ee9e
VINCENT <vwal290@aucklanduni.ac.nz>
2025-03-30 15:51:29 +1300

    Enhance Button styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button>Click me</Button>
+        <Button className="bg-blue-500 text-white text-xl rounded-xl py-4 px-8 hover:bg-blue-600 active:bg-blue-700 focus:ring-4 focus:ring-blue-300">
+          Click me
+        </Button>
       </div>
     </>
   );
