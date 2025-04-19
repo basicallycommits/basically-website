daa763675d8261fa41005d33ffcf8ba1e27f47b0
VINCENT <vwal290@aucklanduni.ac.nz>
2025-04-19 23:08:37 +1200

    chore: fix projects skills used

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -13,12 +13,7 @@ const Projects = () => {
         "https://images.unsplash.com/photo-1724770388447-30b015a5cbb6?q=80&w=1939&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       date: "Expected Start: Jul 2025",
       status: "coming-soon",
-      skillsUsed: [
-        "Altium Designer",
-        "Microchip Studio",
-        "Quartus",
-        "QuestaSim",
-      ],
+      skillsUsed: ["Altium Designer", "Microchip Studio", "Proteus", "LTSpice"],
       placeholderImage: true,
     },
     {
@@ -29,7 +24,7 @@ const Projects = () => {
         "https://images.unsplash.com/photo-1597862624292-45748390b00e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       date: "Apr 2025 – Present",
       status: "in-progress",
-      skillsUsed: ["VHDL", "FPGA", "Proteus", "LTSpice"],
+      skillsUsed: ["VHDL", "FPGA", "Quartus", "QuestaSim"],
       placeholderImage: true,
     },
     {
