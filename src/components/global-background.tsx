f10790c5ac9a3b86c0d448f493a2c4e12a575631
VINCENT <vwal290@aucklanduni.ac.nz>
2025-04-19 22:59:39 +1200

    fix: update dark background colors in GlobalBackground component for better contrast

--- src/components/global-background.tsx
+++ src/components/global-background.tsx
@@ -1,7 +1,7 @@
 const GlobalBackground = () => (
   <div
     aria-hidden="true"
-    className="fixed inset-0 -z-10 bg-gradient-to-br from-sky-50 to-white dark:from-[#0c1322] dark:to-[#0c0c0c]"
+    className="fixed inset-0 -z-10 bg-gradient-to-br from-sky-50 to-white dark:from-[#0a101d] dark:to-[#0c0c0c]"
   />
 );
 
